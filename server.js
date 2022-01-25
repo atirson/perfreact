639ecfd5c502bcc81e6a1093964f002c9bdd9c34
atirson <fabiano.oliveira2213@gmail.com>
2022-01-25 19:49:53 -0300

    feat: Created structure to improve performece on ReactJS/NextJS

---
+++ server.js
@@ -0,0 +1,15 @@
+module.exports = () => {
+  const data = {
+    products: []
+  }
+
+  for (let i = 0; i < 100; i++) {
+    data.products.push({
+      id: i + 1,
+      title: `Camiseta ${i + 1}`,
+      price: i * 10
+    })
+  }
+
+  return data
+}
